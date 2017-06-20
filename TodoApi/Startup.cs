cd59c206b3d65b3b954d1568e00cc6ae08567461
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-20 17:28:29 -0500

    initial commit, Models containing class for Todo Item and setting up context for local db storage

---
+++ TodoApi/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {   
+        //cobnfigures services - controller? uses services as its dot notation namespace - kind of like controller as
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
+            services.AddMvc();
+        }
+        //configures the app - uses app as the dot notation namespace - kind of like controller as
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
